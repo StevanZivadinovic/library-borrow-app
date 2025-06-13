c8b444aaa134e217fad5bcdb5b6a86c27c9ba789
StevanZivadinovic <stevanzivadinovic@outlook.com>
2025-06-13 23:47:13 +0200

    add header style

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,11 @@
+import { Header } from '@/components/Header';
 import React, { ReactNode } from 'react'
 
  const Layout = ({children}:{children:ReactNode}) => {
   return (
     <main className=':root'>
         <div className="mx-auto max-w-7xl text-center">
-            Header
+            <Header/>
             <div className="mt-20 pb-20">{children}</div>
         </div>
     </main>

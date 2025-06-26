506391d64610348ca6babd3e9338e07f2b253aa2
StevanZivadinovic <stevanzivadinovic@outlook.com>
2025-06-26 18:41:23 +0200

    add style to signin and login page and added react hook form and zod

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,29 @@
+import Image from 'next/image'
 import React, { ReactNode } from 'react'
 
 const Layout = ({children}: {children: ReactNode}) => {
   return (
-    <div>{children}</div>
+    <main className="auth-container flex">
+<section className='auth-form max-w-[50%] p-[40px] mt-[10vh] mx-[5vw] bg-gradient-to-b
+ from-[#12141D] to-[#12151F] rounded-lg '>
+<div className="auth-box w-[80%]">
+  <div className="flex flex-row gap-3">
+    <Image src={'icons/logo.svg'} alt='logo' width={37} height={37}></Image>
+    <h1 className='text-2xl font-semibold text-white'>BookWise</h1>
+  </div>
+      <div className='mt-5'>{children}</div>
+</div>
+</section>
+<section className='auth-illustration w-[50%]'>
+  <Image
+  src={'/images/auth-illustration.png'}
+  alt='auth-illustration'
+  width={1000}
+  height={1000}
+  className='size-full object-cover'
+  ></Image>
+</section>
+    </main>
   )
 }
 
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,17 @@
+import AuthForm from '@/components/AuthForm';
 import React from 'react'
 
  const SignInPage = () => {
   return (
-    <div>SignInPage</div>
+    <div>
+      <div className="">
+      <h1 className="text-3xl font-semibold">Create Your Library Account</h1>
+      <p className='text-[18px] mt-2 text-[var(--basic-gray)] !font-thin'>Please complete all fields and upload a valid university ID to gain
+        access to the library
+    </p>
+      </div>
+      <AuthForm/>
+    </div>
   )
 }
 export default SignInPage;

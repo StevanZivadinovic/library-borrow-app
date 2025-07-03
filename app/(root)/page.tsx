aed6ea122602b4f8e4c7b7abb158bd5cd9aa84a5
StevanZivadinovic <stevanzivadinovic@outlook.com>
2025-07-03 22:36:30 +0200

    add neon and drizzle

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { sampleBooks } from "@/constants";
-
+import 'dotenv/config'
 
 export default function Home() {
   return    <>

a3d4b794e769ce00ac75c063217747dd828efcb6
StevanZivadinovic <stevanzivadinovic@outlook.com>
2025-07-28 18:14:31 +0200

    added ngrok

--- config.ts
+++ config.ts
@@ -16,7 +16,7 @@ const config = {
     upstash: {
       redisUrl: process.env.UPSTASH_REDIS_REST_URL!,
       redisToken: process.env.UPSTASH_REDIS_REST_TOKEN!,
-      qstashUrl: process.env.QSTASH_URL!,
+      // qstashUrl: process.env.QSTASH_URL!,
       qstashToken: process.env.QSTASH_TOKEN!,
     },
     // resendToken: process.env.RESEND_TOKEN!,
